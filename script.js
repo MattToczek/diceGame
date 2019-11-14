900631009ff229e3b78b3f6a2a36ef0bf48695cd
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-14 09:44:18 +0000

    extra stats added to game

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ class Player {
 const currentPlayer = [];
 const scoreReads =[];
 
-
+let player1;
+let player2;
 let diceDiv = document.getElementById("dice");
 let rollBtn = document.getElementById("roll");
 let score = document.getElementsByClassName("scores");
@@ -34,7 +35,23 @@ let p1Label = document.getElementById("p1Label");
 let p2Label = document.getElementById("p2Label");
 let p1SAndL = document.getElementById("p1SAndL");
 let p2SAndL = document.getElementById("p2SAndL");
+let p1Stats = document.getElementById("p1Stats");
+let p1Lowest = document.getElementById("p1Lowest");
+let p1LowLab = document.getElementById("p1LowestLab");
+let p1WinLab = document.getElementById("p1WinLab");
+let p1WinRead = document.getElementById("p1WinRead");
+let p1Wins = document.getElementById("p1Wins");
+let p2Stats = document.getElementById("p2Stats");
+let p2Lowest = document.getElementById("p2Lowest");
+let p2LowLab = document.getElementById("p2LowestLab");
+let p2WinLab = document.getElementById("p2WinLab");
+let p2WinRead = document.getElementById("p2WinRead");
+let p2Wins = document.getElementById("p2Wins");
 
+p1Lowest.value = 0;
+p2Lowest.value = 0;
+p1Wins.value = 0;
+p2Wins.value = 0;
 
 
 for (let i = 0; i < score.length; i++) {
@@ -61,7 +78,8 @@ let switchPlayers = () => {
 
 let setName = () => {
     player.textContent = `${currentPlayer[0].name} it's your turn!`;
-    score.value = currentPlayer[0].currNum
+    score.value = currentPlayer[0].currNum;
+
 }
 
 let gameOverRun = () => {
@@ -79,34 +97,9 @@ let gameOverRun = () => {
     inputScreen.style.display = "flex";
 }
 
-let checkScore = () => {
-    for (let i = 0; i < currentPlayer.length; i++) {
-        if (currentPlayer[i].currNum >= winningScore && currentPlayer.length > 1) {
-            winRead = `<h2>${currentPlayer[i].name.toUpperCase()} IS THE WINNER!!!</h2>
-                        <p>They won with ${currentPlayer[i].rolls} rolls.</p>
-                        <button id="reset">Reset</button>`
-                        inputScreen.style.textAlign = "center";
-                        gameOverRun();
-                        currentPlayer[i].win()
-        } else if (currentPlayer[i].currNum >= winningScore){
-            winRead = `<h2>You got to ${winningScore} with ${currentPlayer[i].rolls} rolls.</h2>
-                        <button id="reset">Reset</button>`
-                        inputScreen.style.textAlign = "center";
-                        gameOverRun();
-        }
 
-        
-        
-    }
-}
 
-let addScore = (num) => {
-    currentPlayer[0].currNum += num; 
-    scoreReads[0].value = currentPlayer[0].currNum;
-    currentPlayer[0].rolls ++;
-    score.value = currentPlayer[0].currNum
-    checkScore();
-}
+
 
 let getDice = () => {                          //randomly adds score and displays relevant dice image
     let num = diceRoll();
@@ -117,13 +110,19 @@ let getDice = () => {                          //randomly adds score and display
 }
 
 playBtn.addEventListener('click', ()=> {
-    let player1 = new Player(p1Name.value);
-    let player2 = new Player(p2Name.value);
+    player1 = new Player(p1Name.value);
+    player2 = new Player(p2Name.value);
     
     currentPlayer.push(player1);
     p1Label.textContent = player1.name
     scoreReads.push(p1Score);
     p1SAndL.style.display = "inline-flex";
+    let p1StatHead = document.createElement("LI");
+    p1StatHead.textContent = `${player1.name}'s rolls:`;
+    p1LowestLab.textContent = `${player1.name}'s lowest number of rolls:`;
+    p1WinLab.textContent = `${player1.name}'s wins:`;
+    p1Stats.style.display = "flex";
+
 
 
     if(player2.name != "" ){
@@ -131,17 +130,76 @@ playBtn.addEventListener('click', ()=> {
         p2Label.textContent = player2.name
         scoreReads.push(p2Score);
         p2SAndL.style.display = "inline-flex";
+        let p2StatHead = document.createElement("LI");
+        p2StatHead.textContent = `${player2.name}'s rolls:`;
+        p2LowestLab.textContent = `${player2.name}'s lowest number of rolls:`;
+        p2WinLab.textContent = `${player2.name}'s wins:`;
+        p2Stats.style.display = "flex";
 
     }
 
     scoresDiv.style.display = "flex";
     scoresDiv.style.justifyContent = "space-around";
     inputScreen.style.display = "none";
-    console.log(currentPlayer)
+    
     setName();
     
 });
 
+let addScore = (num) => {
+    currentPlayer[0].currNum += num; 
+    scoreReads[0].value = currentPlayer[0].currNum;
+    currentPlayer[0].rolls ++;
+    score.value = currentPlayer[0].currNum
+
+    checkScore();
+}
+
+let updateRolls = ()=> {
+    if(currentPlayer[0] == player1 && (p1Lowest.value == 0 || p1Lowest.value > player1.rolls)){
+        p1Lowest.value = currentPlayer[0].rolls;
+    } else if (p1Lowest.value == 0 || p2Lowest.value > player2.rolls){
+        p2Lowest.value = currentPlayer[0].rolls;
+    }
+
+}
+
+let upWins = () => {
+    if(currentPlayer[0] == player1){
+        p1Wins.value ++;
+    }else{
+        p2Wins.value ++;
+    }
+}
+
+let checkScore = () => {
+    for (let i = 0; i < currentPlayer.length; i++) {
+        
+        if (currentPlayer[i].currNum >= winningScore && currentPlayer.length > 1) {
+            winRead = `<h2>${currentPlayer[i].name.toUpperCase()} IS THE WINNER!!!</h2>
+                        <p>They won with ${currentPlayer[i].rolls} rolls.</p>
+                        <button id="reset">Reset</button>`
+                        inputScreen.style.textAlign = "center";
+                        updateRolls();
+                        upWins();
+                        gameOverRun();
+                        currentPlayer[i].win()
+
+        } else if (currentPlayer[i].currNum >= winningScore){
+            winRead = `<h2>You got to ${winningScore} with ${currentPlayer[i].rolls} rolls.</h2>
+                        <button id="reset">Reset</p1button>`
+                        inputScreen.style.textAlign = "center";
+                        updateRolls();
+                        p1Wins.value ++;
+                        gameOverRun();
+        }
+
+        
+        
+    }
+}
+
 rollBtn.addEventListener('click', ()=> {
     getDice();
+
 });
